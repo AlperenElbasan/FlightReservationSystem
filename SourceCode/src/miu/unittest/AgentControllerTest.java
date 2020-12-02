b4f7b8955becb40878e5fc0bf487be4c62229ed5
TungTop <stefanitvn@gmail.com>
2020-12-02 09:00:25 -0600

    update unittest

--- SourceCode/src/miu/unittest/AgentControllerTest.java
+++ SourceCode/src/miu/unittest/AgentControllerTest.java
@@ -1,14 +1,26 @@
 package miu.unittest;
 
+import miu.StorageHandler;
+import miu.controllers.AgentController;
+import miu.models.*;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AgentControllerTest {
-
+    AgentController agentCtr = new AgentController();
     @Test
     void makeReservation() {
-       //TODO:
+        Agent agent = new Agent();
+        agent.addPassenger(StorageHandler.getRandomPassenger(1));
+        agent.addPassenger(StorageHandler.getRandomPassenger(2));
+        List<FlightInstance> flightInstances = StorageHandler.generateListFlightInstance(5);
+        Passenger passenger = StorageHandler.getRandomPassenger(2);
+        Reservation re = AgentController.createReservation(agent, passenger, flightInstances);
+
+//        assertEquals(re, re.getReservationCode(), "true");
     }
 
     @org.junit.jupiter.api.Test
