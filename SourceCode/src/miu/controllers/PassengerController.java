519e9fdb7cdbc6741061dc44e3a1971dd1ee1fa5
Bayartsogt Yadamsuren <tsogoo_97@icloud.com>
2020-12-01 21:43:59 -0600

    adding viewReservations for agent and passenger

--- SourceCode/src/miu/controllers/PassengerController.java
+++ SourceCode/src/miu/controllers/PassengerController.java
@@ -18,7 +18,8 @@ public class PassengerController {
     public void getListAirport(){
 
     }
-
+    
+    // added by Bayartsogt
     public static List<FlightInstance> getFlightsOnDate(String departureAirportName, String arrivalAirportName, Date date) {
         
         List<FlightInstance> flightInstancesFound = ArrayList<FlightInstance>();
@@ -38,11 +39,12 @@ public class PassengerController {
         return flightInstancesFound;
     }
 
+    // added by Bayartsogt
     public static List<Reservation> getOwnReservation(Passenger passenger) {
     	return passenger.getReservation();
     }
 
-    // currently it is just printing
+    // currently it is just printing // added by Bayartsogt
     public static void getDetailsOfReservation(Reservation reservation) {
         System.out.println("=================")
         System.out.println("Agent ID:" + reservation.agent)
